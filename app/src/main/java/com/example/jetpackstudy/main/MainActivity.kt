1eead4b7e02198e9f2dfd952c5faae6dbf655277
KwakYongwoo <kkolbuyw@gmail.com>
2020-02-29 02:53:36 +0900

    change colorPrimary & PrimaryDark(dark) && change backPressed event(double tab to close app)

--- app/src/main/java/com/example/jetpackstudy/main/MainActivity.kt
+++ app/src/main/java/com/example/jetpackstudy/main/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.jetpackstudy.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.jetpackstudy.R
 import com.example.jetpackstudy.databinding.ActivityMainBinding
@@ -12,14 +14,14 @@ import com.example.jetpackstudy.main.setting.SettingFragment
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val fragment1 = SearchFragment()
+    private val fragment2 = ListFragment()
+    private val fragment3 = SettingFragment()
+    private var doubleBackToExitPressedOnce = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        val fragment1 = SearchFragment()
-        val fragment2 = ListFragment()
-        val fragment3 = SettingFragment()
 
         supportActionBar?.title = "Search"
         supportFragmentManager.beginTransaction().replace(R.id.main_frameLayout, fragment1).commitAllowingStateLoss()
@@ -51,6 +53,20 @@ class MainActivity : AppCompatActivity() {
                     return@setOnNavigationItemSelectedListener true
                 }
             }
+        }
+    }
+
+    override fun onBackPressed() {
+        if(supportFragmentManager.findFragmentById(R.id.main_frameLayout) !is SearchFragment) {
+            binding.mainBottomNavigation.selectedItemId = R.id.navigation_search
+        } else {
+            if(doubleBackToExitPressedOnce) {
+                super.onBackPressed()
+                return
+            }
+            doubleBackToExitPressedOnce = true
+            Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+            Handler().postDelayed({ doubleBackToExitPressedOnce = false }, 2000)
         }
     }
 }
